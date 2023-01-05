8dcbb7b8ed36accbb4fc975db623a640fa036d10
rinkuberde <rinkuberde@gmail.com>
2023-01-05 10:14:02 +0530

    [Rinku]-wlcome message

---
+++ EmpWageProblem/EmpWageProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee management");
+        }
+    }
+}
